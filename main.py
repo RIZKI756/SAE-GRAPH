3426baac33829ad1f713d8b6aea347fd18797c65
Yamixors <diswallah10@gmail.com>
2026-06-11 16:44:43 +0200

    résolution de problème  code et git

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from model import Grille
 from view import MainWindow
+from controller import GameController
 
 def main():
     app = QApplication(sys.argv)
     
-    # Initialisation de la vue seule
-    view = MainWindow()
-    view.show()
+    # 1. On instancie le modèle (les données de la grille)
+    model = Grille()
     
+    # 2. On instancie la vue principale (l'IHM)
+    view = MainWindow()
+    
+    # 3. On crée le contrôleur qui connecte le modèle et la vue
+    controller = GameController(model, view)
+    
+    # 4. On affiche la fenêtre et on lance la boucle d'exécution PyQt
+    view.show()
     sys.exit(app.exec())
 
 if __name__ == '__main__':
